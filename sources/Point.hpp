3a7f27e216dd72edfdeb96eda4d8ed9741b2461b
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-05-01 15:19:27 +0300

    finish point header

--- sources/Point.hpp
+++ sources/Point.hpp
@@ -2,11 +2,28 @@
 #define POINT_HPP
 
 #include <iostream>
-namespace ariel{
 
+namespace ariel {
+    // This class represents a point 
+    class Point {
+    private:
+        double x; // The x-coordinate of the point.
+        double y; // The y-coordinate of the point.
+    
+    public:
+        /*Constructor for creating a new point with the given x and y coordinates.*/
+        Point(double x, double y);
+        
+        /*Calculates the Euclidean distance between this point and another point.
+          The other point is passed as a parameter.*/
+        double distance(Point other);
+        
+        /* Prints the coordinates of the given point to the console.*/
+        void print(Point point);
+        
+        /*The function returns the closest point to the destination point, - which is at most the given distance from the source point*/
+        Point moveTowards(Point source, Point destination, double distance);
+    };
+}
 
-
-
-
-};
 #endif
