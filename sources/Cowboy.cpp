4c44895d6d271646855fd140c0dee81db07ef48d
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-05-02 19:16:15 +0300

    cowboy

---
+++ sources/Cowboy.cpp
@@ -0,0 +1,46 @@
+#include "Cowboy.hpp"
+
+using namespace ariel;
+using namespace std;
+
+Cowboy::Cowboy(string name, Point& location):name(name), location(location){ }
+
+void Cowboy::shoot(Character& enemy){
+
+}
+
+void Cowboy::reload(){
+
+}
+
+bool Cowboy::hasboolets() const {
+    return bullet > 0;
+}
+
+void Cowboy::reload() {
+    bullet = 6;
+}
+
+bool Cowboy::isAlive() const {
+    return Character::isAlive();
+}
+
+double Cowboy::distance(const Character& other) const {
+    return 0;
+}
+
+void Cowboy::hit(int num) {
+}
+
+string Cowboy::getName() const {
+    return getName();
+}
+
+Point Cowboy::getLocation() const {
+    return getLocation();
+}
+
+void Cowboy::print() {
+    // cout << "Cowboy " << getName() << " is at location (" << getLocation().getX() << ", " << getLocation().getY() << ")" << endl;
+}
+
--- sources/Cowboy.hpp
+++ sources/Cowboy.hpp
@@ -15,6 +15,18 @@ namespace ariel {
             void shoot(Character& enemy);
             bool hasboolets() const;
             void reload();
+
+             /* Returns true if the character has more than 0 hit points, and false otherwise*/
+            bool isAlive() const override;
+            /*Calculates the distance between this character and another character.*/
+            double distance(const Character& other) const override;
+            /* Decreases the character's hit points by the given number.*/
+            void hit(int num) override;
+            /*Returns the character's name*/
+            string getName() const override;
+            /*Returns the character's current location.*/
+            Point getLocation() const override;
+            /*Prints the character's name and location to the console.*/
             void print() override;
     };
 }
