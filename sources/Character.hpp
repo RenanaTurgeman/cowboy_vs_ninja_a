543dcd54d3548ebce770ba0ed977962aed92ee55
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-05-02 18:54:29 +0300

    make virtual for override

--- sources/Character.hpp
+++ sources/Character.hpp
@@ -22,15 +22,15 @@ namespace ariel {
             Character(string name, Point& location, int hit_point);
             ~Character() = default; 
             /* Returns true if the character has more than 0 hit points, and false otherwise*/
-            bool isAlive() const;
+            virtual bool isAlive() const;
             /*Calculates the distance between this character and another character.*/
-            double distance(const Character& other) const;
+            virtual double distance(const Character& other) const;
             /* Decreases the character's hit points by the given number.*/
-            void hit(int num);
+            virtual void hit(int num);
             /*Returns the character's name*/
-            string getName() const;
+            virtual string getName() const;
             /*Returns the character's current location.*/
-            Point getLocation() const;
+            virtual Point getLocation() const;
             /*Prints the character's name and location to the console.*/
             virtual void print();
     };
