9adf818fdb32a6c54de21bf436048a1ecf662b2a
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-05-03 15:30:31 +0300

    fix demo

--- Demo.cpp
+++ Demo.cpp
@@ -13,41 +13,41 @@
 using namespace std;
 
 #include "sources/Team.hpp" //no need for other includes
+#include "sources/TrainedNinja.hpp"
 
 using namespace ariel;
 
 
 int main() {
-    cout << "hello world" << endl;
- //    Point a(32.3,44),b(1.3,3.5);
-//    assert(a.distance(b) == b.distance(a));
-//    Cowboy tom = new Cowboy("Tom", a);
-//    OldNinja sushi = new OldNinja("sushi", b);
-//    tom.shoot(sushi);
-//    cout << tom->Print() <<endl;
-//
-//    sushi.move(tom);
-//    sushi.slash(tom);
-//
-//    Team a(tom);
-//    a.add(new YountNinja("Yogi", Point(64,57)));
-//
-//    // Team b(&tom); should throw tom is already in team a
-//
-//     Team b(&sushi);
-//     b.add(new TrainedNinja("Hikari", Point(12,81)));
-//
-//
-//     while(a.stillAlive() > 0 && b.stillAlive() > 0){
-//        a.attack(&b);
-//        b.attack(&a);
-//        a.print();
-//        b.print();
-//     }
-//
-//     if (a.stillAlive() > 0) cout << "winner is a" << endl;
-//     else cout << "winner is b" << endl;
+    Point a(32.3,44),b(1.3,3.5);
+    assert(a.distance(b) == b.distance(a));
+    Cowboy tom =  Cowboy("Tom", a);
+    OldNinja sushi =  OldNinja("sushi", b);
+    tom.shoot(sushi);
+    cout << tom.Print() <<endl;
 
-     return 0; // no memory issues. Team should free the memory of its members. both a and b teams are on the stack. 
+    sushi.move(tom);
+    sushi.slash(tom);
+
+    Team a1(tom);
+    a1.add( YountNinja("Yogi", Point(64,57)));
+
+    // Team b(&tom); should throw tom is already in team a
+
+    Team b2(sushi);
+    b2.add( TrainedNinja("Hikari", Point(12,81)));
+
+
+    while(a1.stillAlive() > 0 && b2.stillAlive() > 0){
+        a1.attack(&b2);
+        b2.attack(&a1);
+        a1.print();
+        b2.print();
+    }
+
+    if (a1.stillAlive() > 0) cout << "winner is a" << endl;
+    else cout << "winner is b" << endl;
+
+    return 0; // no memory issues. Team should free the memory of its members. both a and b teams are on the stack.
 
 }
