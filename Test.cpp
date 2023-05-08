9ec772da238507a104a8d62769906458d7c1c3f4
RenanaTurgeman <renanaturgeman058@gmail.com>
2023-05-08 13:54:02 +0300

    character tests

--- Test.cpp
+++ Test.cpp
@@ -53,3 +53,44 @@ TEST_SUITE("Test Point Class") {
     }
 
 } // end of test suite
+
+TEST_SUITE("Test Character Class") {
+
+    TEST_CASE("Character default constructor creates a valid object") {
+        Point location{1, 1};
+        Character c{"Bob", location, 10};
+        CHECK(c.getName() == "Bob");
+        CHECK(c.getLocation().distance(location) == 0);
+        CHECK(c.isAlive()== true);
+    }
+
+    TEST_CASE("Character distance calculation is correct") {
+        Point location1{1, 1};
+        Point location2{4, 5};
+        Character c1{"Bob", location1, 10};
+        Character c2{"Alice", location2, 10};
+        double expected_distance = location1.distance(location2);
+        CHECK(c1.distance(c2) == expected_distance);
+    }
+
+    TEST_CASE("Character hit points can be decreased") {
+        Point location{1, 1};
+        Character c{"Bob", location, 10};
+        int num_hits = 3;
+        c.hit(num_hits);
+        CHECK(c.isAlive()== true);
+        CHECK(c.getName() == "Bob");
+        CHECK(c.getLocation().distance(location) == 0);
+        CHECK(c.isAlive()== true);
+//        CHECK(c.getHitPoints() == 10 - num_hits);
+    }
+
+    TEST_CASE("Character hit points reach zero when hit enough times") {
+        Point location{1, 1};
+        Character c{"Bob", location, 2};
+        int num_hits = 3;
+        c.hit(num_hits);
+        CHECK(c.isAlive()== false);
+    }
+
+}// end suit character
